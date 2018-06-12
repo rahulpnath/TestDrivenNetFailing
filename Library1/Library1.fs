93de48aa5c927b095284029adc4bfe045a19d96b
Rahul P Nath <rahulpnath@gmail.com>
2018-06-12 17:18:41 +1000

    initial

--- Library1/Library1.fs
+++ Library1/Library1.fs
@@ -1,4 +1,35 @@
-﻿namespace Library1
+﻿namespace Ploeh.Samples
 
-type Class1() = 
-    member this.X = "F#"
+open System
+
+module Messaging =    
+    type Envelope<'a> = {
+        Id      : Guid
+        Created : DateTimeOffset
+        Item    : 'a }
+
+    let envelop getId getTime item = {
+        Id = Guid "1CF889F8-201F-44DF-BC86-77227651D3EE"
+        Created = DateTimeOffset.MinValue
+        Item = item }
+
+module MessagingTests =
+    open Xunit
+
+    type Foo = { Text : string; Number : int }
+
+    [<Fact>]
+    let ``enevelope returns correct results`` () =
+        let getId _ = Guid "1CF889F8-201F-44DF-BC86-77227651D3EE"
+        let getTime _ = DateTimeOffset( 636322011751405346L, 
+                                        TimeSpan.FromHours(-4.0) )
+        let item = { Text = "Bar"; Number = 42 }
+
+        let actual = Messaging.envelop getId getTime item
+
+        Assert.Equal ( Guid "1CF889F8-201F-44DF-BC86-77227651D3EE",
+                       actual.Id )
+        Assert.Equal ( DateTimeOffset( 636322011751405346L, 
+                                       TimeSpan.FromHours(-4.0) ),
+                       actual.Created )
+        Assert.Equal ( item, actual.Item ) 
